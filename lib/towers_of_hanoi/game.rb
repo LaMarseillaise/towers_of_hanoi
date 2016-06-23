5648c0d28ec15351a8741c4e115fd791b521d2a2
Andur Carr <carr.andur@gmail.com>
2016-06-23 16:07:45 -0700

    Build game

---
+++ lib/towers_of_hanoi/game.rb
@@ -0,0 +1,28 @@
+module TowersOfHanoi
+  class Game
+    attr_reader :moves, :board
+
+    def initialize(board: TowersOfHanoi::Board.new(bricks: 3), moves: [])
+      @board = board
+      @moves = moves
+
+      @moves.each { |move| move.make(@board) }
+    end
+
+    def number_of_bricks
+      @board.bricks
+    end
+
+    def turns
+      @moves.length
+    end
+
+    def over?
+      @board.tower(3).full?
+    end
+
+    def make_move(move)
+      @moves << move if move.make(@board)
+    end
+  end
+end
---
+++ spec/game_spec.rb
@@ -0,0 +1,30 @@
+require "spec_helper"
+
+describe TowersOfHanoi::Game do
+  let(:move) { TowersOfHanoi::Move.new(origin: 1, destination: 3) }
+  let(:board){ TowersOfHanoi::Board.new(bricks: 1) }
+  let(:game) { TowersOfHanoi::Game.new(board: board) }
+  let(:endgame){ TowersOfHanoi::Game.new(board: board, moves: [move]) }
+
+  describe "#over?" do
+    it "begins false" do
+      expect(game.over?).to be false
+    end
+
+    it "is true when all tiles are on the last tower" do
+      expect(endgame.over?).to be true
+    end
+  end
+
+  describe "#make_move" do
+    it "adds the move to the list of moves" do
+      expect{game.make_move(move)}.to change(game.moves, :length).by 1
+    end
+
+    it "changes the state of the board" do
+      game.make_move(move)
+      expect(game.board.tower(3).full?).to be true
+      expect(game.board.tower(1).full?).to be false
+    end
+  end
+end
